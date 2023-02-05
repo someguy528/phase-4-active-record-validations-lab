1eeb9ffb41f39f7b36a93c83aaa2620c6f04d6c8
Brandon Castro <castroyou999@gmail.com>
2023-02-05 02:09:10 -0500

    commit tree bb5efa0295ea2086860d86df35d8a1433f7d6382

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        unless title =~ /Won't Believe/i || title =~ /Secret/i ||title =~ /Top [number]/i || title =~ /Guess/i
+            errors.add(:title, "Must be clickbait!")
+        end
+    end
 end
